c55e92aff90febe072c19ef4374b14869f39bd40
camilavheuer <cbvillalbaheuer@gmail.com>
2026-02-05 11:30:16 -0300

    feat: add GlobalHandlerException to AuthService

--- auth-service/src/main/java/com/camicompany/auth_service/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/com/camicompany/auth_service/dto/LoginRequestDTO.java
@@ -1,7 +1,11 @@
 package com.camicompany.auth_service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequestDTO(@NotBlank String username,
-                              @NotBlank String password) {
+public record LoginRequestDTO(
+        @Schema(example="user1")
+        @NotBlank String username,
+        @Schema(example="user1Password")
+        @NotBlank String password) {
 }
